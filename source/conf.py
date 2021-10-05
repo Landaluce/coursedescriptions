7f50d24faf659733e3d1d68c38fa424fc6eaf694
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-10-04 21:19:34 -0500

    Implement external link opening

--- source/conf.py
+++ source/conf.py
@@ -478,3 +478,31 @@ epub_basename = "LoyolaComputerScienceAcademicPrograms"
 
 # Allow duplicate toc entries.
 # epub_tocdup = True
+
+# Open external links in a new window
+# https://stackoverflow.com/a/67153583
+
+from sphinx.writers.html import HTMLTranslator
+from sphinx.writers.html5 import HTML5Translator
+from sphinx.util.docutils import is_html5_writer_available
+
+
+class PatchedHTMLTranslator(
+    HTML5Translator if is_html5_writer_available() else HTMLTranslator
+):
+    def starttag(self, node, tagname, *args, **attrs):
+        if (
+            tagname == "a"
+            and "target" not in attrs
+            and (
+                "external" in attrs.get("class", "")
+                or "external" in attrs.get("classes", [])
+            )
+        ):
+            attrs["target"] = "_blank"
+            attrs["ref"] = "noopener noreferrer"
+        return super().starttag(node, tagname, *args, **attrs)
+
+
+def setup(app):
+    app.set_translator("html", PatchedHTMLTranslator)
