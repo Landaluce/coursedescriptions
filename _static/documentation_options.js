f212b1ff21b39c7fbc447c30a7bce8b1ac4260da
NicholasSynovic <NicholasSynovic@users.noreply.github.com>
2021-11-03 01:55:20 +0000

    deploy: 5f6ddcf8d9a990d85e6a5f775940348cad3db261

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Updated 2021-10-19',
+    VERSION: 'Updated 2021-11-03',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
